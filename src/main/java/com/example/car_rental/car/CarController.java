7636fca460259abb2351cca83805cb28f0102438
Barkóczi Balázs <f5hx5h@inf.elte.hu>
2024-07-06 18:30:15 +0200

    Add CarController, CarRepository, CarService, Functionalities: get all cars, add new car

---
+++ src/main/java/com/example/car_rental/car/CarController.java
@@ -0,0 +1,41 @@
+package com.example.car_rental.car;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CarController {
+
+    private static final String VIEWS_CAR_CREATE_OR_UPDATE_FORM = "cars/createOrUpdateOwnerForm";
+
+    private final CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping("/cars")
+    public String getAllCars(Model model){
+        model.addAttribute("allCars",carService.getAllCars());
+        return "carsList";
+    }
+
+    @GetMapping("/cars/new")
+    public String initNewCar(Model model){
+        model.addAttribute("car", new Car());
+        return VIEWS_CAR_CREATE_OR_UPDATE_FORM;
+    }
+
+    @PostMapping("/cars/new")
+    public String processNewCar(@ModelAttribute("car") Car car){
+        carService.saveCar(car);
+        return "redirect:/cars";
+    }
+
+
+
+}
---
+++ src/main/java/com/example/car_rental/car/CarService.java
@@ -0,0 +1,28 @@
+package com.example.car_rental.car;
+
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CarService {
+
+    private final CarRepository carRepository;
+
+    public CarService(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+    //GET
+    public List<Car> getAllCars() {
+        return (List<Car>) carRepository.findAll();
+    }
+
+    //POST
+    public void saveCar(Car car) {
+        carRepository.save(car);
+    }
+
+    
+}
